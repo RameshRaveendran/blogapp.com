55f5c620a9c35b50486c3b79da7a3eecce83a791
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-01-28 10:39:19 +0530

    main file check

---
+++ app.js
@@ -0,0 +1,49 @@
+// require environment variables
+require("dotenv").config();
+
+// main requires
+const express = require ('express');
+const session = require('express-session');
+const path = require('path');
+
+// local requires
+const connectDB = require('./config/db');
+
+// app
+const app = express();
+
+// database Connection
+connectDB();
+
+// middleware
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, "public")));
+
+// session
+app.use(
+    session({
+        secret:process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false
+    })
+);
+
+// viewengine setup
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// routes
+app.get("/",(req , res) => {
+    res.send('Blog app Running');
+});
+
+// vercel exportation
+module.exports = app;
+
+//local server runner
+if(require.main === module) {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`)
+    })
+}
